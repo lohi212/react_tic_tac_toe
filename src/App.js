cc4dd7405dbe4027c606bcbddc0dd5747039cb03
lohi212 <suvvarilohita@gmail.com>
2023-04-10 16:08:11 +0530

    feat(clear): clear button

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
+        <button onClick={() => setValues(new Array(9))}>clear</button>
         <Winner
           user={user}
           values={values}
